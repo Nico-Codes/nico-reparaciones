acf59473ca7a3076236b6f144247dd86e91d8e04
Nico-Codes <nicolas5905code@gmail.com>
2025-12-22 14:58:57 -0300

    V21-Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>@yield('title', config('app.name', 'NicoReparaciones'))</title>
+  <link rel="icon" href="{{ asset('favicon.ico') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+  <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+  <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+  <meta name="theme-color" content="#0ea5e9">
+
 
   @php
     $hotPath = public_path('hot');
